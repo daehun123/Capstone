96d7486ed35fe939adb5307413a3705df7a007c7
crol0101@naver.com <crol0101@naver.com>
2025-03-25 13:36:28 +0900

    이메일 인증 ing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
 import { getData } from "../util/api";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const nav = useNavigate();
+
   const [data, setData] = useState();
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await getData();
-        setData(response.data);
+        const res = await getData();
+
+        if (res.status === 200) {
+          setData(res.data);
+          console.log(res.data.massage);
+        }
       } catch (error) {
         alert("접근 불가! 로그인하세요");
+        nav("/", { replace: true });
       }
     };
     fetchData();
-  }, []);
+  }, [nav]);
   return <div>Home</div>;
 };
 
--- src/util/api.js
+++ src/util/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://13.124.205.29:8080";
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_URL,
@@ -13,8 +13,9 @@ const api = axios.create({
 //로그인 요청, 토큰 발급 후 로컬 저장소에 저장
 export const login = async (email, password) => {
   const response = await api.post(`/login`, { email, password });
-  localStorage.setItem("accessToken", response.data.accessToken);
-  localStorage.setItem("refreshToken", response.data.refreshToken);
+  const { accessToken, refreshToken } = response.data.data;
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("refreshToken", refreshToken);
   return response;
 };
 
@@ -28,7 +29,10 @@ export const signup = async (name, email, password, birthdate) => {
   });
   return response;
 };
-
+//이메일 인증 코드 받아오기
+export const getCode = async (email) => {
+  const response = await api.post("/", { email });
+};
 //로그아웃 요청
 export const logout = async () => {
   localStorage.removeItem("accessToken");
@@ -39,15 +43,14 @@ export const logout = async () => {
 //데이터 요청
 export const getData = async () => {
   const token = localStorage.getItem("accessToken");
-
   try {
-    return api.get(`/`, {
+    return api.get(`/hello`, {
       headers: { Authorization: `Bearer ${token}` },
     });
   } catch (error) {
     if (error.response && error.response.status === 401) {
       const re_token = await refreshToken();
-      return await axios.get(`/`, {
+      return await axios.get(`/hello`, {
         headers: { Authorization: `Bearer ${re_token}` },
       });
     } else if (error.response && error.response.status === 403) {
@@ -56,9 +59,11 @@ export const getData = async () => {
   }
 };
 
-export const refreshToken = async () => {
+//토큰 재발급
+const refreshToken = async () => {
   const response = await api.get(`/refresh-token`);
-  localStorage.setItem("accessToken", response.data.accessToken);
-  localStorage.setItem("refreshToken", response.data.refreshToken);
-  return response.data.accessToken;
+  const { accessToken, refreshToken } = response.data.data;
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("refreshToken", refreshToken);
+  return accessToken;
 };
