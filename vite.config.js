394cbeb6f4dd12528aefbcd15aeb26ddd932a613
crol0101@naver.com <crol0101@naver.com>
2025-05-13 00:32:00 +0900

    Fix: manifest json 깨짐 해결

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,45 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
-
-      manifest: "/manifest.webmanifest",
-
+      manifest: {
+        id: "/",
+        name: "Capstone",
+        short_name: "Capstone",
+        description: "졸업 프로젝트",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
       workbox: {
         globPatterns: ["**/*.{js,css,html,svg,png,ico,webmanifest}"],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
       },
-
       devOptions: {
         enabled: false,
         navigateFallback: "index.html",
